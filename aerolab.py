5eda8e1b31ccdd4d649748b2f5fe7b8faa0c13eb
Christopher Kakerow <chris.kakerow@krizbiz.de>
2025-01-08 20:42:07 +0100

    feat(aerolab): add main code structure of aerolab

---
+++ aerolab.py
@@ -0,0 +1,34 @@
+import argparse
+
+
+def main(args):
+
+
+    return None
+
+
+def compute_distances():
+    return None
+
+
+def compute_complexities():
+    return None
+
+
+def compute_average_precision():
+    return None
+
+
+def combine_distances_and_complexities():
+    return None
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment-id", default="default")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main(parse_args())
